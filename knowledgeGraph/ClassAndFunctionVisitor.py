292bb5ac2d4f2918a30cac84b4b22fe5f635423c
sedorfer <selinaschoe@gmx.de>
2024-12-15 18:42:46 +0100

    type hinting and commenting

--- knowledgeGraph/ClassAndFunctionVisitor.py
+++ knowledgeGraph/ClassAndFunctionVisitor.py
@@ -1,19 +1,17 @@
 import ast
-import re
 
 
-# Define a visitor class that will collect class and function information
+# Visitor class that will collect class and function information from the AST
 class ClassAndFunctionVisitor(ast.NodeVisitor):
-    # TO DO: Write tests
-    def __init__(self, comments) -> None:
+    def __init__(self, comments: list) -> None:
         self.classes = []
         self.functions = []
         self.comments = comments
-        # stack to keep track of class_nesting
-        self.class_stack = []
-        super().__init__()
+        self.class_stack = [] # stack to keep track of nested classes
+        super().__init__() # inherits the base functionalities of ast.NodeVisitor
 
-    def get_name(self, node): 
+    def get_name(self, node) -> str: 
+        # parses the name of the node depending on the node type
         if isinstance(node, ast.Name): 
             name = node.id
         elif isinstance(node, ast.Constant):
@@ -26,22 +24,23 @@ class ClassAndFunctionVisitor(ast.NodeVisitor):
             name = "No value"
         return name
 
-    def parse_parameters(self, node, comments): 
+    def parse_parameters(self, node, comments: list) -> list: 
+        # parses the input parameters of a function
         default_values = [self.get_name(d) for d in node.args.defaults]
-        params_with_defaults = ["No default"] * (len(node.args.args) - len(default_values)) + default_values
+        params_with_defaults = ["No default"] * (len(node.args.args) - len(default_values)) + default_values    # add default values
         params = []
         for i, arg in enumerate(node.args.args):
             param_info = {
                 "name": arg.arg,
                 "type": self.get_name(arg.annotation),
                 "default": params_with_defaults[i], 
-                "comment": comments.get(arg.arg, "")
+                "comment": comments.get(arg.arg, "")    # if there is no comment, an empty string is added 
             }
             params.append(param_info)
         return params
 
 
-    def parse_function(self, node):
+    def parse_function(self, node) -> dict:
         parsed_comments = self.parse_comments(node.lineno)
         return_type = self.get_name(node.returns)
         params = self.parse_parameters(node, parsed_comments["param"])
@@ -53,7 +52,7 @@ class ClassAndFunctionVisitor(ast.NodeVisitor):
             "comment": parsed_comments.get("brief", "")
         }
     
-    def parse_attribute(self, node):
+    def parse_attribute(self, node) -> dict:
         parsed_comments = self.parse_comments(node.lineno)
         return {
             "name": node.targets[0].id,
@@ -61,7 +60,8 @@ class ClassAndFunctionVisitor(ast.NodeVisitor):
             "comment": parsed_comments.get("brief", "")
         }
     
-    def traverse_body(self, info, node): 
+    def traverse_body(self, info: dict, node): 
+        # traverses the body of a node and parses the child nodes
         for elem in node.body:
             if isinstance(elem, ast.FunctionDef):
                 info['methods'].append(self.parse_function(elem))
@@ -71,26 +71,28 @@ class ClassAndFunctionVisitor(ast.NodeVisitor):
                 nested_class_info = self.parse_class(elem)
                 info['nested_classes'].append(nested_class_info)
 
-    def get_associated_comments(self, lineno): 
+    def get_associated_comments(self, lineno: int) -> list: 
+        # gets the comments that belong to a specific node within the AST
         associated_comments = []
-        for line, comment in reversed(self.comments): 
-            if line < lineno: 
-                if comment.startswith("##"):
+        for line, comment in reversed(self.comments):   # start from the bottom
+            if line < lineno:   # look at the comments directly above the class/function etc.
+                if comment.startswith("##"): # marks the end of the comment belonging to this node
                     break
                 if comment == "#": 
                     continue
-                associated_comments.append(comment.strip("#").strip())
-        associated_comments.reverse()
+                associated_comments.append(comment.strip("#").strip())  # remove the unnecessary whitespace and #
+        associated_comments.reverse()   # need to reverse as we started from the bottom 
         return associated_comments
 
 
-    def parse_comments(self, lineno):  
+    def parse_comments(self, lineno: int) -> dict:  
         associated_comments = self.get_associated_comments(lineno=lineno)
         parsed_comments = {"param": {}} 
         current_tag = None
         current_param = None
 
         for comment in associated_comments:
+            # the comments start with certain tags that indicate what they will describe
             if comment.startswith("\\brief"):
                 current_tag = "brief"
                 parsed_comments["brief"] = comment.strip("\\brief").strip()
@@ -108,6 +110,7 @@ class ClassAndFunctionVisitor(ast.NodeVisitor):
                 parsed_comments["return"] = comment.strip("\\return").strip()
 
             else:
+                # comments will go over multiple lines
                 if current_tag == "brief":
                     parsed_comments["brief"] += " " + comment.strip()  
                 elif current_tag == "param" and current_param:
@@ -118,11 +121,11 @@ class ClassAndFunctionVisitor(ast.NodeVisitor):
         return parsed_comments
             
     
-    def parse_class(self, node): 
-        parsed_comments = self.parse_comments(node.lineno)
+    def parse_class(self, node) -> dict: 
+        parsed_comments = self.parse_comments(node.lineno)  # get associated comments
         class_info = {
             'name': node.name,
-            "bases": [self.get_name(b) for b in node.bases],
+            "bases": [self.get_name(b) for b in node.bases],    # base it inherits from
             "decorators": [self.get_name(d) for d in node.decorator_list],
             'methods': [],
             "class_attributes": [],
@@ -132,15 +135,17 @@ class ClassAndFunctionVisitor(ast.NodeVisitor):
         self.traverse_body(class_info, node)
         return class_info
 
-    def visit_ClassDef(self, node):
-        self.class_stack.append(node.name)
+    def visit_ClassDef(self, node) -> None: 
+        # needed to overwrite the default function from ast
+        self.class_stack.append(node.name)  # handles nested classes
         class_info = self.parse_class(node)
         if len(self.class_stack) == 1:  
             self.classes.append(class_info)
         self.generic_visit(node)
         self.class_stack.pop()
     
-    def visit_FunctionDef(self, node):
+    def visit_FunctionDef(self, node) -> None:
+        # needed to overwrite the default function from ast
         if not self.class_stack: 
             self.functions.append(self.parse_function(node))
         self.generic_visit(node)
